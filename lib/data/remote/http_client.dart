aa5d4b298aa95544f87443ac81993c11d831de11
Marcos Calvo García <macalvo@hiberus.com>
2022-09-21 08:57:22 +0200

    HttpClient was switched to singleton

--- lib/data/remote/http_client.dart
+++ lib/data/remote/http_client.dart
@@ -7,9 +7,15 @@ import 'credentials_wallet.dart';
 class HttpClient {
   final Dio dio = Dio();
 
+  static final HttpClient _httpClient = HttpClient._internal();
+
+  factory HttpClient() {
+    return _httpClient;
+  }
+
   // https://github.com/flutterchina/dio/blob/develop/example/lib/queued_interceptor_crsftoken.dart
   // https://gist.github.com/TimurMukhortov/a1c9819e3779015e54bc3964b7d2308a
-  HttpClient() {
+  HttpClient._internal() {
     dio.interceptors
       ..add(DioLoggingInterceptor())
       ..add(QueuedInterceptorsWrapper(onRequest: (options, handler) async {
