0a76c138c4c73fad90365d28f29af00287877f88
Marcos Calvo García <macalvo@hiberus.com>
2023-05-10 11:33:17 +0200

    Custom serializer was removed

--- lib/di/app_modules.dart
+++ lib/di/app_modules.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_mvvm/data/artists/artists_data_impl.dart';
 import 'package:flutter_mvvm/data/artists/remote/artists_remote_impl.dart';
-import 'package:flutter_mvvm/data/artists/serializer/artist_serializer.dart';
 import 'package:flutter_mvvm/data/auth/auth_data_impl.dart';
 import 'package:flutter_mvvm/domain/artists_repository.dart';
 import 'package:get_it/get_it.dart';
@@ -31,8 +30,7 @@ class AppModules {
   }
 
   _setupArtistsModule() {
-    inject.registerSingleton(ArtistSerializer());
-    inject.registerFactory(() => ArtistsRemoteImpl(inject.get(), inject.get()));
+    inject.registerFactory(() => ArtistsRemoteImpl(inject.get()));
     inject.registerFactory<ArtistsRepository>(
         () => ArtistsDataImpl(inject.get()));
     inject.registerFactory(() => ArtistsViewModel(inject.get()));
